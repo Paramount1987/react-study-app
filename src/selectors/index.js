eeb0c8c917538de39572fbf6888f33df0e034c77
Paramount1987 <Paramount7891@gmail.com>
2018-02-13 17:47:26 +0300

    += selector for Comment

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -3,6 +3,8 @@ import {createSelector}   from 'reselect';
 
 const filtersGetter = state => state.filters;
 const articlesGetter = state => state.articles;
+const commentsGetter = state => state.comments;
+const idGetter = (state, props) => props.id;
 
 export const filterArticlesSelector = createSelector(filtersGetter, articlesGetter, (filters, articles) => {
     const {filterByValue} = filters;
@@ -16,4 +18,8 @@ export const filterArticlesSelector = createSelector(filtersGetter, articlesGett
         return (!filterByValue.length || filterByValue.includes(article.id)) &&
             (!from || !to || (articleTime.diff(timeFrom) > 0 && articleTime.diff(timeTo) < 0))
     });
+});
+
+export const commentSelectorFactory = () => createSelector(commentsGetter, idGetter, (comments, id) => {
+    return comments.find(comment => comment.id === id)
 });
