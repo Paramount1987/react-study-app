bbc29d85665a88b6c021d920ab5b7c4b866dbcc4
Paramount1987 <Paramount7891@gmail.com>
2018-02-15 17:05:43 +0300

    += simple Route

--- src/App.js
+++ src/App.js
@@ -8,20 +8,30 @@ import UserForm from './components/UserForm'
 import Filter   from './components/Filters'
 import Counter  from './components/Counter'
 
+import {HashRouter as Router, Route, NavLink}   from 'react-router-dom'
+
 class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <Header />
-                </header>
-                <div>
-                    <Filter />
-                    <ArticleList/>
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <Header />
+                    </header>
+                    <nav>
+                        <h2>Main menu</h2>
+                        <div><NavLink to="/counter" activeClassName="selected">Counter</NavLink></div>
+                        <div><NavLink to="/filter" activeClassName="selected">Filter</NavLink></div>
+                        <div><NavLink to="/articles" activeClassName="selected">Articles</NavLink></div>
+                    </nav>
+
+                    <Route path = "/counter" component = {Counter} />
+                    <Route path = "/filter" component = {Filter} />
+                    <Route path = "/articles" component = {ArticleList} />
                 </div>
-            </div>
+            </Router>
         );
     }
 }
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -24,7 +24,7 @@ class ArticleList extends Component {
 
     componentDidMount() {
         const {loading, loaded, loadAllArticles} = this.props;
-        if(!loaded || !loading) loadAllArticles();
+        if(!loaded && !loading) loadAllArticles();
     }
 
     render() {
