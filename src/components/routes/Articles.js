bbe5a7c3252061b1d81917bc4df2ecde921eaeac
Paramount1987 <Paramount7891@gmail.com>
2018-02-15 18:55:48 +0300

    += render Articles index

--- src/components/routes/Articles.js
+++ src/components/routes/Articles.js
@@ -8,6 +8,7 @@ class Articles extends Component {
     render() {
         return (
             <div>
+                <Route path="/articles" render={this.getIndex} exact/>
                 <table>
                     <tbody>
                         <tr>
@@ -22,8 +23,11 @@ class Articles extends Component {
 
     getArticle = ({match}) => {
         const {id} = match.params;
-        return <h2>Please select article</h2>;
         return <Article id={id} key={id} isOpen/>
+    }
+
+    getIndex = () => {
+        return <h2>Please select article</h2>;
     }
 }
 
