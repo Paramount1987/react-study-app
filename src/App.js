95e6023f74fa2acb320a02775355d77f370e71b1
Paramount1987 <Paramount7891@gmail.com>
2018-02-15 19:25:58 +0300

    += not found page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css'
 
 import  Header  from './components/Header'
 import Articles   from './components/routes/Articles'
+import NewArticle   from './components/routes/NewArticle'
+import NotFound   from './components/routes/NotFound'
 import UserForm from './components/UserForm'
 import Filter   from './components/Filters'
 import Counter  from './components/Counter'
 
-import {HashRouter as Router, Route, NavLink}   from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, NavLink}   from 'react-router-dom'
 
 class App extends Component {
 
@@ -26,10 +28,13 @@ class App extends Component {
                         <div><NavLink to="/filter" activeClassName="selected">Filter</NavLink></div>
                         <div><NavLink to="/articles" activeClassName="selected">Articles</NavLink></div>
                     </nav>
-
-                    <Route path = "/counter" component = {Counter} />
-                    <Route path = "/filter" component = {Filter} />
-                    <Route path = "/articles" component = {Articles} />
+                    <Switch>
+                        <Route path = "/counter" component = {Counter} />
+                        <Route path = "/filter" component = {Filter} />
+                        <Route path = "/articles/new" component = {NewArticle} />
+                        <Route path = "/articles" component = {Articles} />
+                        <Route path = "*" component = {NotFound} />
+                    </Switch>
                 </div>
             </Router>
         );
---
+++ src/components/routes/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NotFound(props) {
+    return (
+        <div>Not Found</div>
+    );
+}
+
+NotFound.propTypes = {};
+NotFound.defaultProps = {};
+
+export default NotFound;
