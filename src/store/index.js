e35bbd02d154e2b051b86e10680967a49ce6e7e4
Paramount1987 <Paramount7891@gmail.com>
2018-02-16 12:49:48 +0300

    += redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import reducer  from '../reducer';
 import logger   from '../middlewares/logger';
 import randomId from '../middlewares/randomId';
@@ -7,7 +7,13 @@ import thunk    from 'redux-thunk';
 import {routerMiddleware}   from 'react-router-redux';
 import history  from '../history';
 
-const enhancer = applyMiddleware(thunk, routerMiddleware(history), randomId, api, logger);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+/*const enhancer = applyMiddleware(thunk, routerMiddleware(history), randomId, api, logger);*/
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk, routerMiddleware(history), randomId, api, logger)
+);
 
 const store = createStore(reducer, {}, enhancer);
 
